2ebca3990491b6bb9541d0f4001a8e642afe2f3f
JongHun Lee <jonghun@JongHunui-MacBookAir.local>
2023-09-16 18:09:07 +0900

    feat: 메인화면 카테고리 선택 ui 구현

--- dockggu/lib/page/home.dart
+++ dockggu/lib/page/home.dart
@@ -1,59 +1,141 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<String> items = [
+    '📚 전체',
+    '🕵🏻‍♂️ 소설',
+    '📕 에세이',
+    '🗺 여행',
+    '👤 인문학',
+    '🎨 디자인',
+    '🧐 철학',
+    '🗿 역사',
+    '🎫 예술/문화',
+    '📈 경제/경영',
+    '👩🏻‍⚖️ 사회/정치',
+    '✍🏻 시',
+    '🛍 라이프스타일',
+    '🏗 건축',
+    '🧬 과학',
+    '🖥 컴퓨터/IT',
+    '💪 건강/운동',
+    '👨🏻‍💻 자기계발',
+    '💵 재테크',
+    '📱 마케팅'
+  ];
 
   Widget _banner() {
     return Container(
-      color: Color(0xffFFF5DA),
-      height: 120,
+      color: const Color(0xffFFF5DA),
+      height: 110,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SizedBox(
+          const SizedBox(
             width: 20,
           ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
-              Text(
+              const Text(
                 '이닛 님의 취향에 맞는 책 추천해드려요',
                 style: TextStyle(
                     color: Colors.black,
                     fontSize: 18,
                     fontWeight: FontWeight.bold),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              Text("AI 기반으로 찾아드려요"),
+              const Text("AI 기반으로 찾아드려요"),
               Row(
                 children: [
-                  SizedBox(width: 170,),
+                  const SizedBox(
+                    width: 170,
+                  ),
                   Container(
-                        alignment: Alignment.center,
-                        width: 110,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Color(0xffFFD66C)
-                        ),
-                        child: Text('책 추천 받기 >',style: TextStyle(color: Colors.white),),
-                      ),
+                    alignment: Alignment.center,
+                    width: 110,
+                    height: 30,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color(0xffFFD66C)),
+                    child: const Text(
+                      '책 추천 받기 >',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ],
               )
             ],
           ),
-          SizedBox(
+          const SizedBox(
             width: 25,
           ),
-          Text(
+          const Text(
             '📚',
             style: TextStyle(fontSize: 45),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _bookathon_title() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, top: 30, right: 20),
+      child: Column(
+        children: [
+          const Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                '북커톤 🏃🏼',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text('완독을 목표로 함께 달려봐요'),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Wrap(
+            direction: Axis.horizontal, // 나열 방향
+            alignment: WrapAlignment.start, // 정렬 방식
+            spacing: 8, // 좌우 간격
+            runSpacing: 10, // 상하 간격
+            children: List.generate(items.length, (index) {
+              return Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Color(0xffCBC6C6))),
+                height: 30,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    items[index],
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ), // 각 아이템에 다른 텍스트 설정
+              );
+            }),
+          ),
         ],
       ),
     );
@@ -64,10 +146,11 @@ class Home extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: [
-        SizedBox(
+        const SizedBox(
           height: 70,
         ),
-        _banner()
+        _banner(),
+        _bookathon_title(),
       ],
     ));
   }
